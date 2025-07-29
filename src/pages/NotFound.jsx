c7038d779b686e53d07199625a0814025b39c18d
PaulMakoriMachuka <paulmachukamakori@gmail.com>
2025-07-29 10:44:49 +0300

    main

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+// src/pages/NotFound.jsx
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you’re looking for does not exist.</p>
+    </div>
+  );
+}
+
+export default NotFound;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,31 @@
-
+// src/routes.jsx
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Directors from "./pages/Directors";
+import Actors from "./pages/Actors";
+import NotFound from "./pages/NotFound";
 
 const routes = [
-   
-  ];
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/movies/:id",
+    element: <Movie />,
+  },
+  {
+    path: "/directors",
+    element: <Directors />,
+  },
+  {
+    path: "/actors",
+    element: <Actors />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+];
 
 export default routes;
